20756e64e23c4f55bbb958bca24de6c45cdf6bce
KrajewskiMaciej <makros123456@gmail.com>
2025-09-05 18:45:02 +0200

    Style Działają

--- digitalwars.client/vite.config.ts
+++ digitalwars.client/vite.config.ts
@@ -3,29 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import Icons from 'unplugin-icons/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    Icons({
-        autoInstall: true
-      })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-    css: {
-    postcss: {
-      plugins: [], // Możemy zostawić to puste, aby zmusić Vite do użycia `postcss.config.js`
-    },
-  },
-
-  server: {
-    port: 5517
-  }
 })
