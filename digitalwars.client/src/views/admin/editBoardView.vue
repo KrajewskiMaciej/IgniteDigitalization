298dffb5af98205a13f605f882adfc2ca1031831
Łukasz Przewodowski <lukasz.przewodowski@itm.local>
2025-11-20 22:31:05 +0100

    Obsluzono blad zwiazany z usuwaniem uzywanej planszy

--- digitalwars.client/src/views/admin/editBoardView.vue
+++ digitalwars.client/src/views/admin/editBoardView.vue
@@ -108,9 +108,13 @@ import boardInfo from '@/components/editBoard/boardInfo.vue'
 import boardColorSettings from '@/components/editBoard/boardColorSettings.vue'
 import boardLabelsEditors from '@/components/editBoard/boardLabelsEditors.vue'
 import boardDescriptions from '@/components/editBoard/boardDescriptions.vue'
+import { useI18n } from 'vue-i18n'
 
 import apiConfig from '@/services/apiConfig'
 import apiService from '@/services/apiServices'
+
+
+const { t } = useI18n();
 
 // --- INTERFEJSY ---
 
@@ -349,8 +353,13 @@ const handleDeleteBoard = () => {
           activeView.value = 'add'
         }
       } catch (error: any) {
-        console.error('Błąd podczas usuwania planszy:', error.response?.data || error.message)
-        const errorMessage = error.response?.data?.title || error.response?.data || error.message
+        if (error.response?.status === 409) {
+          toast.warning(
+            t('errorBoardDeleteConflict')
+          );
+          return;
+        }
+        const errorMessage = error.response?.data || error.message || 'Nieznany błąd'
         toast.error(`Błąd usuwania: ${errorMessage}`)
       }
     },
