ed24ae07f2bc77e5cec1ec353544d9f9ae36ccbe
Maciej Krajewski <maciej.krajewski@itm.com.pl>
2026-06-11 13:39:50 +0200

    Poprawiono sktypt budowania

--- ignitedigitalization.client/src/components/footers/myFooter.vue
+++ ignitedigitalization.client/src/components/footers/myFooter.vue
@@ -1,5 +1,11 @@
 <template>
   <footer>
+    <div class="flex justify-center items-center text-xs md:text-sm text-slate-400 w-full bg-primary py-2 shadow-[0_-2px_4px_theme(colors.primary.400/0.5)]">
+      <div>
+        {{ t('version') }}: {{ version }}
+      </div>
+
+    </div>
     <div
       class="flex justify-center items-center text-xs md:text-sm text-white w-full bg-secondary py-2 shadow-[0_-2px_4px_theme(colors.primary.400/0.5)]"
     >
@@ -17,4 +23,6 @@ import { useI18n } from 'vue-i18n'
 import logo from '@/assets/logos/Zasob_24x.jpg'
 
 const { t } = useI18n()
+
+const version = (import.meta.env.VITE_APP_VERSION as string) || 'dev';
 </script>
